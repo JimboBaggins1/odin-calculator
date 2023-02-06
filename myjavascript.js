494a92d732a00d45b4b6723de9df0ce6a22e234e
JimboBaggins <james.anderson98@outlook.com>
2023-02-06 09:23:06 +0000

    Add start of function to create calculator UI

--- myjavascript.js
+++ myjavascript.js
@@ -1,5 +1,21 @@
 //myjavascript.js
 
+//----------------------Create calculator display-------------------------//
+const container = document.querySelector('#container');
+
+const createCalc = function() {
+    for (let i = 0; i < 10; i++) {
+        const buttons = document.createElement('button');
+        buttons.setAttribute('class', 'buttons');
+        container.appendChild('buttons');
+    }
+}
+
+
+
+
+
+
 
 //----------------------Operator Functions-------------------------//
 
@@ -24,3 +40,11 @@ const divide = function(num1, num2) {
     return num1 / num2;
 };
 
+
+//----------------------Operate-------------------------//
+
+
+// Takes two numbers and and operator callback
+const operate = function(operator, num1, num2) {
+    return operator(num1, num2);
+};
