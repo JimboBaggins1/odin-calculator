a4f745983112a071ea3072e98616a90a6ead39dd
JimboBaggins <james.anderson98@outlook.com>
2023-02-06 10:54:37 +0000

    Add function to create calculator display

--- myjavascript.js
+++ myjavascript.js
@@ -1,17 +1,30 @@
 //myjavascript.js
 
 //----------------------Create calculator display-------------------------//
+const buttonIDArray = ['AC', 'plus-minus', 'mod', 'divide', 'seven', 'eight', 'nine', 'multiply',
+                    'four', 'five', 'six', 'minus', 'one', 'two', 'three', 'add', 'zero', 'point',
+                    'equals'];
+
+const buttonArray = ['AC', '+/-', '%', '/', '7', '8', '9', '*',
+                    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.',
+                    '='];
+
 const container = document.querySelector('#container');
 
-const createCalc = function() {
-    for (let i = 0; i < 10; i++) {
+const createCalc = function(idArray, buttonArray) {
+    const display = document.createElement('div');
+    display.setAttribute('id', 'display');
+    container.appendChild(display);
+    for (let i = 0; i < 19; i++) {
         const buttons = document.createElement('button');
         buttons.setAttribute('class', 'buttons');
-        container.appendChild('buttons');
+        buttons.setAttribute('id', `${idArray[i]}`);
+        buttons.textContent = `${buttonArray[i]}`;
+        container.appendChild(buttons);
     }
 }
 
-
+createCalc(buttonIDArray, buttonArray);
 
 
 
