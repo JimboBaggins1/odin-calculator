a131b092d8afd96c06244e40b1509b58ce92fa5f
JimboBaggins <james.anderson98@outlook.com>
2023-02-06 09:04:50 +0000

    Create functions for basic maths operators

---
+++ myjavascript.js
@@ -0,0 +1,26 @@
+//myjavascript.js
+
+
+//----------------------Operator Functions-------------------------//
+
+
+// Add
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+
+// Subtract
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+// Multiply
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+// Divide
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
