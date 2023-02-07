fe6626aaa22b6a6cfa1d97c97d62657533528718
JimboBaggins <james.anderson98@outlook.com>
2023-02-07 12:16:36 +0000

    Fix function to calculate multiple operations

--- myjavascript.js
+++ myjavascript.js
@@ -1,5 +1,15 @@
 //myjavascript.js
 
+
+let displayValue = 0;
+let operand1 = 0;
+let operand2 = 0;
+let operator = function(num1,num2) {
+  return num1 + num2;
+};
+let storage1 = null;
+let storage2 = null;
+
 //----------------------Create calculator display-------------------------//
 const keyCodes = ['c', '@', '%', '/', '7', '8', '9', '*', '4', 
                     '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
@@ -52,28 +62,81 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
+const clear = function (num1, num2) {
+  return 0;
+}
+
 //----------------------Operate-------------------------//
 
 // Takes two numbers and and operator callback
-const operate = function (operator, num1, num2) {
+const operate = function (operator, num1 = 0, num2 = 0) {
+  display.textContent = `${operator(num1, num2)}`;
   return operator(num1, num2);
 };
 
 //----------------------Functions to populate display when key is pressed-------------------------//
+const isOperatorClicked = function (keys) {
+  switch (keys) {
+    case "+":
+      if (storage1 === null) {
+        storage1 = display.textContent.replace(/\W|_/g, "");
+      } else {
+        storage2 = display.textContent.replace(/\W|_/g, "");
+        storage1 = operate(operator, Number(storage1), Number(storage2));
+      }
+      console.log(storage1);
+      console.log(storage2);
+      operator = add;
+      display.textContent = "";
+      break;
+    case "-":
+      if (storage1 === null) {
+        storage1 = display.textContent.replace(/\W|_/g, "");
+      } else {
+        storage2 = display.textContent.replace(/\W|_/g, "");
+        storage1 = operate(operator, Number(storage1), Number(storage2));
+      }
+      console.log(storage1);
+      console.log(storage2);
+      operator = subtract;
+      display.textContent = "";
+      break;
+    case "*":
+      if (storage1 === null) {
+        storage1 = display.textContent.replace(/\W|_/g, "");
+      } else {
+        storage2 = display.textContent.replace(/\W|_/g, "");
+        storage1 = operate(operator, Number(storage1), Number(storage2));
+      }
+      console.log(storage1);
+      console.log(storage2);
+      operator = multiply;
+      display.textContent = "";
+      break;
+    case "/":
+      if (storage1 === null) {
+        storage1 = display.textContent.replace(/\W|_/g, "");
+      } else {
+        storage2 = display.textContent.replace(/\W|_/g, "");
+        storage1 = operate(operator, Number(storage1), Number(storage2));
+      }
+      console.log(storage1);
+      console.log(storage2);
+      operator = divide;
+      display.textContent = "";
+      break;
+    case "=":
+      storage2 = display.textContent.replace(/\W|_/g, "");
+      storage1 = operate(operator, Number(storage1), Number(storage2));
+      display.textContent = storage1;
+      storage1 = null;
+  }
+};
+
 // Populate display
 const populateDisplay = function (keys) {
-    if (keys === 'AC') {
-        display.textContent = '0';
-    }
-    if (display.textContent.length <= 9) {
-        if (keys === 'AC') {
-            display.textContent = '0';
-        } else {
-            display.textContent += `${keys}`;
-        }
-} 
-  let storedDisplay = display.textContent;
-  console.log(storedDisplay);
+  display.textContent += `${keys}`;
+  isOperatorClicked(keys);
 };
 
 // Look for key press
@@ -96,5 +159,10 @@ for (let i = 0; i < buttons.length; i++) {
 
 
 
-
-
+// take user input
+// if user presses operator key
+  // store display value in storage2
+  // perform first operation on storage1 and storage2
+  // store result in storage1
+  // store operator in operator storage
+  // clear display
